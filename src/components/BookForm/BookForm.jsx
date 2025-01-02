aae3f9e756a5d05b971ec0cc5352310d92718da8
Raiyan <ray99scholars@gmail.com>
2025-01-02 18:14:20 +0600

    Added details page

---
+++ src/components/BookForm/BookForm.jsx
@@ -0,0 +1,122 @@
+import PropTypes from 'prop-types';
+import Select from 'react-select'
+
+const BookForm = ({card}) => {
+
+    console.log(card);
+    
+
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+      ]
+
+    return (
+        <div>
+
+            <div className="card bg-base-100 lg:w-3/4 mx-auto">
+            <form className="card-body p-0">
+                
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Service ID</span>
+                </label>
+                <input type="text" value="12345" className="input input-bordered" readOnly />
+                </div>
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Service Name</span>
+                </label>
+                <input type="text" defaultValue={card.serviceName} className="input input-bordered" readOnly />
+                </div>
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Service Image</span>
+                </label>
+                <input type="url" defaultValue={card.serviceImage} className="input input-bordered" readOnly />
+                </div>
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Provider Email</span>
+                </label>
+                <input type="email" defaultValue={card.providerEmail} className="input input-bordered" readOnly />
+                </div>
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Provider Name</span>
+                </label>
+                <input type="text" defaultValue={card.providerName} className="input input-bordered" readOnly />
+                </div>
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Current User Email</span>
+                </label>
+                <input type="email" defaultValue={card.providerEmail} className="input input-bordered" readOnly />
+                </div>
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Current User Name</span>
+                </label>
+                <input type="text" defaultValue={card.providerName} className="input input-bordered" readOnly />
+                </div>
+
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text text-blue-600">Service Taking Date</span>
+                </label>
+                <input type="date" className="input input-bordered" required />
+                </div>
+
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text text-blue-600">Area</span>
+                </label>
+
+                <Select options={options} required />
+                </div>
+
+
+
+
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Price</span>
+                </label>
+                <input type="text" defaultValue={card.price} className="input input-bordered" readOnly />
+                </div>
+
+
+                <div className="flex justify-center items-center mt-6">
+                <button className="btn btn-primary w-1/2">Purchase</button>
+                </div>
+                
+            </form>
+
+            <div className="modal-action justify-center">
+                <form method="dialog" className="w-1/2">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn bg-red-500 w-full">Cancel</button>
+                </form>
+            </div>
+            </div>
+
+            
+        </div>
+    );
+};
+
+BookForm.propTypes = {
+    card: PropTypes.object,
+}
+
+export default BookForm;
--- src/components/DetailCard/DetailCard.jsx
+++ src/components/DetailCard/DetailCard.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
+import BookForm from "../BookForm/BookForm";
 
 
 const DetailCard = ({card = {}}) => {
-    console.log(card?.serviceImage);
 
-
+    window.scrollTo(0, 10);
     
     return (
         <div className="w-11/12 mx-auto">
@@ -24,6 +24,7 @@ const DetailCard = ({card = {}}) => {
             <div className="divider"></div>
 
             <h1 className="text-5xl text-center font-bold">Service Details</h1>
+
             <div className="my-12 grid grid-cols-1 lg:grid-cols-2 items-center">
 
                 <img src={card.serviceImage} alt="" className='object-cover h-48 md:h-96 w-full rounded-2xl justify-self-center lg:justify-self-start' />
@@ -44,13 +45,69 @@ const DetailCard = ({card = {}}) => {
                         <p className="font-semibold my-2 lg:my-0">{card.description}</p>
                         <h2 className="font-medium">Price: {card.price}$</h2>
                         <div className="card-actions justify-center">
-                            <button to='/detail' className="btn btn-primary mt-2 w-1/2 lg:w-full lg:my-0">Book Now</button>
+                            <button to='/detail' className="btn btn-primary mt-2 w-1/2 lg:w-full lg:my-0" onClick={()=>document.getElementById('my_modal_5').showModal()}>Book Now</button>
                         </div>
                     </div>
 
             </div>
 
 
+
+
+
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle ">
+                <div className="modal-box !max-w-none">
+                    <h3 className="font-bold text-2xl text-center">Confirm Your Service Booking (Edit Date & Area)</h3>
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <BookForm card={card}></BookForm>
+                </div>
+            </dialog>
+
+
+
+
+
+            {/* Swal.fire({
+                title: "Submit your Github username",
+                input: "text",
+                inputAttributes: {
+                    autocapitalize: "off"
+                },
+                showCancelButton: true,
+                confirmButtonText: "Look up",
+                showLoaderOnConfirm: true,
+                preConfirm: async (login) => {
+                    try {
+                    const githubUrl = `
+                        https://api.github.com/users/${login}
+                    `;
+                    const response = await fetch(githubUrl);
+                    if (!response.ok) {
+                        return Swal.showValidationMessage(`
+                        ${JSON.stringify(await response.json())}
+                        `);
+                    }
+                    return response.json();
+                    } catch (error) {
+                    Swal.showValidationMessage(`
+                        Request failed: ${error}
+                    `);
+                    }
+                },
+                allowOutsideClick: () => !Swal.isLoading()
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                    title: `${result.value.login}'s avatar`,
+                    imageUrl: result.value.avatar_url
+                    });
+                }
+                }); */}
 
         </div>
     );
