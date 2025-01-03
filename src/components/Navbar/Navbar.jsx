13b5dae45c07327b1ec20dc925483cad1da553b4
Raiyan <ray99scholars@gmail.com>
2025-01-03 11:31:21 +0600

    Added register page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
     return (
         <div className="mt-8 mb-6 w-11/12 mx-auto">
 
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 shadow-sm">
                 <div className="navbar-start flex-1">
                     <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,50 @@
+import { Link } from "react-router-dom";
+
 
 
 const Register = () => {
     return (
         <div>
+
+<div className="min-h-screen flex flex-col justify-center items-center w-11/12 mx-auto mb-8">
+                <h1 className="text-4xl my-4"> Register now </h1>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form className="card-body text-black pb-0">
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name="name" placeholder="User Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo-URL</span>
+                        </label>
+                        <input type="text" name="photo" placeholder="Photo-URL" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-2">
+                        <button className="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+
+                    <h2 className="px-8 my-4 text-center text-black">Already have an account? then <Link to='/login' className="text-blue-600">Login</Link></h2>
+                </div>
+
+
+            </div>
+
             
         </div>
     );
